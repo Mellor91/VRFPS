70e4cdb54f2652d3d3e13efbd522baa5cac9f446
Mellor91 <j.mellor@live.co.uk>
2017-03-02 22:19:56 +0000

    interaction moved to player object and onscreen controls working

--- Prototyping/Assets/movement.cs
+++ Prototyping/Assets/movement.cs
@@ -8,7 +8,7 @@ public class movement : MonoBehaviour {
     public float drag = 0.5f;
     public float terminalRotationSpeed = 25f;
     public VirtualJoystick joystick;
-    GameObject joystickObj = GameObject.Find("VirtualJoystickContainer");
+    GameObject joystickObj;
 
     private Rigidbody controller;
     private Transform camTransform;
@@ -20,18 +20,25 @@ public class movement : MonoBehaviour {
         controller.maxAngularVelocity = terminalRotationSpeed;
         controller.drag = drag;
         camTransform = Camera.main.transform;
-	}
+        joystickObj = GameObject.Find("VirtualJoystickContainer");
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
         Vector3 move = Vector3.zero;
 
-        move.x = joystick.inputDirection.x;
-        move.z = joystick.inputDirection.z;
-
-      //  move.x = Input.GetAxis("Horizontal");
-      //  move.z = Input.GetAxis("Vertical");
+        if (joystick.inputDirection.x != 0 && joystick.inputDirection.z != 0)
+        {
+            move.x = joystick.inputDirection.x;
+            move.z = joystick.inputDirection.z;
+        }
+        else
+        {
+            move.x = Input.GetAxis("Horizontal");
+            move.z = Input.GetAxis("Vertical");
+        }
+        
 
         if (move.magnitude>1)
         {
@@ -39,9 +46,11 @@ public class movement : MonoBehaviour {
         }
         Vector3 rotatedMovement = camTransform.TransformDirection(move);
         rotatedMovement = new Vector3(rotatedMovement.x, 0, rotatedMovement.z);
+        Debug.Log("Virtual Joystick = " + move);
+
         rotatedMovement = rotatedMovement.normalized * move.magnitude;
 
-        Debug.Log("Rotated Movement Vector = " + rotatedMovement);
+
 
         controller.AddForce(rotatedMovement * moveSpeed);
     }
